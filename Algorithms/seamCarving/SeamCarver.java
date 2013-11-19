02545cf25052dd7e5176c392ab96b090d9b17e84
zhumeng1989 <zhumeng1989@gmail.com>
2013-11-19 17:50:04 +0800

    PA2: Speedup (DP)

--- Algorithms/seamCarving/SeamCarver.java
+++ Algorithms/seamCarving/SeamCarver.java
@@ -1,7 +1,8 @@
 public class SeamCarver {
 
- private Picture pic;
- private double[] energy; 
+    private Picture pic;
+    private double[] energy;
+    private int[] pathTo;
 
     public SeamCarver(Picture picture) {
         pic = new Picture(picture);
@@ -30,7 +31,7 @@ public class SeamCarver {
     }
 
     // energy of pixel at column x and row y
-    public  double energy(int x, int y) {
+    public double energy(int x, int y) {
         if (x < 0 || x >= width() || y < 0 || y >= height())
             throw new IndexOutOfBoundsException();
         if (x == 0 || y == 0 || x == width()-1 || y == height()-1)
@@ -38,76 +39,82 @@ public class SeamCarver {
         return gradient(pic.get(x-1, y), pic.get(x+1, y)) + gradient(pic.get(x, y-1), pic.get(x, y+1));
     }
 
-    private void computeEnergy() {
+    private double energy(int x, int y, int flag) {
+        if (flag == 1)
+            return energy(y, x);
+        else
+            return energy(x, y);
+    }
+
+    private void computeEnergy(int w, int h, int flag) {
         //double maxE = 0;
-        energy = new double[width() * height()];
-        for (int r = 0; r < height(); r++)
-            for (int c = 0; c < width(); c++) {
-                energy[r*width() + c] = energy(c, r);
-                //maxE = Math.max(maxE, energy[r*width() + c]);
+        energy = new double[w*h];
+        for (int r = 0; r < h; r++)
+            for (int c = 0; c < w; c++) {
+                energy[r*w + c] = energy(c, r, flag);
+                //maxE = Math.max(maxE, energy[r*w + c]);
             }
         /*
-        Picture energyPic = new Picture(width(), height());
-        for (int r = 0; r < height(); r++)
-            for (int c = 0; c < width(); c++) {
-                float color = (float)(energy[r*width() + c] / maxE);
+        Picture energyPic = new Picture(w, h);
+        for (int r = 0; r < h; r++)
+            for (int c = 0; c < w; c++) {
+                float color = (float)(energy[r*w + c] / maxE);
                 energyPic.set(c, r, new java.awt.Color(color, color, color));
             }
         energyPic.show();
         */
     }
 
+    private int[] computePath(int w, int h) {
+        pathTo = new int[w*h];
+        for (int i = 0; i < w; i++)
+            pathTo[i] = -1;
+        for (int r = 1, i = w; r < h; r++) {
+            if (energy[i-w] <= energy[i-w+1]) pathTo[i] = i-w;
+            else pathTo[i] = i-w+1;
+            energy[i] += energy[pathTo[i]]; i++;
+            for (int c = 1; c < w-1; c++, i++) {
+                if (energy[i-w-1] <= energy[i-w]) {
+                    if (energy[i-w-1] <= energy[i-w+1]) pathTo[i] = i-w-1;
+                    else pathTo[i] = i-w+1;
+                } else {
+                    if (energy[i-w] <= energy[i-w+1]) pathTo[i] = i-w;
+                    else pathTo[i] = i-w+1;
+                }
+                energy[i] += energy[pathTo[i]];
+            }
+            if (energy[i-w-1] <= energy[i-w]) pathTo[i] = i-w-1;
+            else pathTo[i] = i-w;
+            energy[i] += energy[pathTo[i]]; i++;
+        }
+
+        int pathEnd = w*(h-1);
+        double minE = energy[w*(h-1)];
+        for (int i = w*(h-1); i < w*h; i++) {
+            if (minE > energy[i]) {
+                minE = energy[i];
+                pathEnd = i;
+            }
+        }
+
+        int[] path = new int[h];
+        for (int p = pathEnd; p >= 0; p = pathTo[p])
+            path[p/w] = p % w;
+        return path;
+    }
+
     // sequence of indices for horizontal seam
     public   int[] findHorizontalSeam() {
-        computeEnergy();
-        int s = width() * height();
-        int t = s+1;
-        EdgeWeightedDigraph g = new EdgeWeightedDigraph(t+1);
-        for (int r = 0; r < height(); r++)
-            g.addEdge(new DirectedEdge(s, r, energy[r*width()]));
-        for (int r = 0; r < height(); r++)
-            for (int c = 1; c < width(); c++) {
-                int i = r*width() + c;
-                int j = c*height() + r;
-                if (r > 0) g.addEdge(new DirectedEdge(j-height()-1, j, energy[i]));
-                g.addEdge(new DirectedEdge(j-height(), j, energy[i]));
-                if (r < height()-1) g.addEdge(new DirectedEdge(j-height()+1, j, energy[i]));
-            }
-        for (int r = 0; r < height(); r++)
-            g.addEdge(new DirectedEdge(s-1-r, t, 0));
-        AcyclicSP asp = new AcyclicSP(g, s);
-        int[] seam = new int[width()];
-        int i = 0;
-        for (DirectedEdge e : asp.pathTo(t))
-            if (i < seam.length)
-                seam[i++] = e.to() % height();
-        return seam;
+        int w = height(), h = width();
+        computeEnergy(w, h, 1);
+        return computePath(w, h);
     }
 
     // sequence of indices for vertical seam
     public   int[] findVerticalSeam() {
-        computeEnergy();
-        int s = width() * height();
-        int t = s+1;
-        EdgeWeightedDigraph g = new EdgeWeightedDigraph(t+1);
-        for (int c = 0; c < width(); c++)
-            g.addEdge(new DirectedEdge(s, c, energy[c]));
-        for (int r = 1; r < height(); r++)
-            for (int c = 0; c < width(); c++) {
-                int i = r*width() + c;
-                if (c > 0) g.addEdge(new DirectedEdge(i-width()-1, i, energy[i]));
-                g.addEdge(new DirectedEdge(i-width(), i, energy[i]));
-                if (c < width()-1) g.addEdge(new DirectedEdge(i-width()+1, i, energy[i]));
-            }
-        for (int c = 0; c < width(); c++)
-            g.addEdge(new DirectedEdge(s-1-c, t, 0));
-        AcyclicSP asp = new AcyclicSP(g, s);
-        int[] seam = new int[height()];
-        int i = 0;
-        for (DirectedEdge e : asp.pathTo(t))
-            if (i < seam.length)
-                seam[i++] = e.to() % width();
-        return seam;
+        int w = width(), h = height();
+        computeEnergy(w, h, 0);
+        return computePath(w, h);
     }
 
     // remove horizontal seam from picture
@@ -134,6 +141,8 @@ public class SeamCarver {
                     p.set(c, r, pic.get(c, r+1));
         }
         pic = p;
+        energy = null;
+        pathTo = null;
         //seamPic.show();
     }
 
@@ -161,6 +170,8 @@ public class SeamCarver {
                     p.set(c, r, pic.get(c+1, r));
         }
         pic = p;
+        energy = null;
+        pathTo = null;
         //seamPic.show();
     }
 
@@ -168,9 +179,29 @@ public class SeamCarver {
         Picture picture = new Picture(args[0]);
         picture.show();
         SeamCarver seamCarver = new SeamCarver(picture);
+        long start, end; 
+        start = System.currentTimeMillis();
         for (int i = 0; i < 10; i++)
+            seamCarver.findVerticalSeam();
+        end = System.currentTimeMillis();
+        System.out.println("Time: "+(end-start)+"ms");
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++)
+            seamCarver.findHorizontalSeam();
+        end = System.currentTimeMillis();
+        System.out.println("Time: "+(end-start)+"ms");
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1; i++)
             seamCarver.removeVerticalSeam(seamCarver.findVerticalSeam());
-        for (int i = 0; i < 10; i++)
+        end = System.currentTimeMillis();
+        System.out.println("Time: "+(end-start)+"ms");
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1; i++)
             seamCarver.removeHorizontalSeam(seamCarver.findHorizontalSeam());
+        end = System.currentTimeMillis();
+        System.out.println("Time: "+(end-start)+"ms");
     }
 }
