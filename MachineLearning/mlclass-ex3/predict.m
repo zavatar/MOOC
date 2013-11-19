b5835be751b18feb15f0c1a2a72fc2efb9ea3521
zhumeng1989 <zhumeng1989@gmail.com>
2013-11-19 21:41:51 +0800

    PA3: Finished (Neural Networks)

--- MachineLearning/mlclass-ex3/predict.m
+++ MachineLearning/mlclass-ex3/predict.m
@@ -21,13 +21,13 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(m, 1) X]';
 
+a2 = [ones(1, m); sigmoid(Theta1*a1)];
 
+a3 = sigmoid(Theta2*a2);
 
-
-
-
-
+[h, p] = max(a3', [], 2);
 
 % =========================================================================
 
