535402ed52b4aef8162e296d67386d7c034a6608
surface <zhumeng1989@gmail.com>
2013-12-11 12:59:50 +0800

    PA5: Passed (3-way quicksort circular suffixarray)

--- Algorithms/burrows/CircularSuffixArray.java
+++ Algorithms/burrows/CircularSuffixArray.java
@@ -1,5 +1,6 @@
 public class CircularSuffixArray {
-
+    // cutoff to insertion sort (any value between 0 and 12)
+    private static final int CUTOFF =  5;
     private final char[] text;
     private final int[] index;
     private final int N;
@@ -11,7 +12,37 @@ public class CircularSuffixArray {
         index = new int[N];
         for (int i = 0; i < N; i++)
             index[i] = i;
-        insertion(0, N-1, 0);
+        sort(0, N-1, 0);
+    }
+
+    private char circularText(int i, int d) {
+        return text[ (i + d) % N ];
+    }
+
+    // 3-way string quicksort lo..hi starting at dth character
+    private void sort(int lo, int hi, int d) { 
+        if (d == N) return;
+
+        // cutoff to insertion sort for small subarrays
+        if (hi <= lo + CUTOFF) {
+            insertion(lo, hi, d);
+            return;
+        }
+
+        int lt = lo, gt = hi;
+        char v = circularText(index[lo], d);
+        int i = lo + 1;
+        while (i <= gt) {
+            int t = circularText(index[i], d);
+            if      (t < v) exch(lt++, i++);
+            else if (t > v) exch(i, gt--);
+            else            i++;
+        }
+
+        // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]. 
+        sort(lo, lt-1, d);
+        sort(lt, gt, d+1);
+        sort(gt+1, hi, d);
     }
 
     // sort from a[lo] to a[hi], starting at the dth character
@@ -24,16 +55,9 @@ public class CircularSuffixArray {
     // is text[i+d..i-1) < text[j+d..i-1) ?
     private boolean less(int i, int j, int d) {
         if (i == j) return false;
-        int _i = i;
-        int _j = j;
-        i = i + d;
-        j = j + d;
-        while (i != _i && j != _j || d == 0) {
-            if (text[i] < text[j]) return true;
-            if (text[i] > text[j]) return false;
-            i = (i + 1) % N;
-            j = (j + 1) % N;
-            d = -1;
+        for (int k = d; k < N; k++) {
+            if (circularText(i, k) < circularText(j, k)) return true;
+            if (circularText(i, k) > circularText(j, k)) return false;
         }
         return i > j;
     }
